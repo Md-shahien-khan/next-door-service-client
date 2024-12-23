0464bdc63779eb4bfc61c066b9ed87dabae30306
Md Shahien Khan <shawonkn58@gmail.com>
2024-12-23 18:10:06 +0000

    added about section

---
+++ src/pages/Home/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import aboutImg from '../../assets/images/gardener.jpg'
+
+const About = () => {
+    return (
+        <div>
+            {/* About*/}
+            <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-8'>
+                <div className='md:w-10/12 mx-auto flex flex-col lg:flex-row justify-between items-center gap-12'>
+                    <div>
+                        <img className='' src={aboutImg} alt="" />
+                    </div>
+                    <div className=''>
+                        <h2 className='text-4xl font-semibold mb-4 md:w-4/5'>From Repairs to Renovations, We have Got You Covered</h2>
+                        <p className='md:w-3/4 text-sm mb-8'>Whether you need plumbing, electrical work, cleaning, repairs, hair cut or even renovation, our team of experienced professionals is here to ensure your home is in expert hands. With a focus on reliability, efficiency, and customer satisfaction, we make it easier than ever to keep your home running smoothly. No job is too big or small—just sit back, relax, and let us handle all your home service needs with care and precision.</p>
+                        <button className='btn bg-orange-500 hover:bg-pink-500 text-white py-2 px-6 rounded-full text-lg hover:transition duration-300 ease-out'>View More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import About from "./About";
 import Banner from "./Banner";
 import DealsNews from "./DealsNews";
 // Home
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <DealsNews></DealsNews>
+            <About></About>
         </div>
     );
 };
