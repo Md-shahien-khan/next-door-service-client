d15c674ca93937776d0d852f182c3703b4b5f04d
Md Shahien Khan <shawonkn58@gmail.com>
2024-12-26 03:39:59 +0000

    made some changes in error page

--- src/pages/errorElement/ErrorElement.jsx
+++ src/pages/errorElement/ErrorElement.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import img from '../../assets/images/404.jpg'
+import imgError from '../../assets/images/404.jpg'
+import { Link } from 'react-router-dom';
 const ErrorElement = () => {
     return (
-        <div>
-            <img src={img} alt="" />
+        <div className='flex justify-center flex-col items-center'>
+            <div>
+                <img className='h-[800px]' src={imgError} alt="" />
+            </div>
+            <Link to='/'><button className='btn btn-info'>Go Home</button></Link>
         </div>
     );
 };
